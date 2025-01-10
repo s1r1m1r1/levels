72f559092fa4f6dbe17f6d136c37224cd1cd5130
Sproute777 <ravarmir@gmail.com>
2025-01-10 19:47:10 +0300

    add decoration

--- lib/flame_select_level_page.dart
+++ lib/flame_select_level_page.dart
@@ -5,11 +5,13 @@ import 'package:flame/components.dart';
 import 'package:flame/effects.dart';
 import 'package:flame/events.dart';
 import 'package:flame/extensions.dart';
-import 'package:flame/experimental.dart' as flameExp;
+import 'package:flame/experimental.dart' as flameExperimental;
 import 'package:flame/game.dart';
 import 'package:flame/layout.dart';
+import 'package:flame/parallax.dart';
 import 'package:flame/rendering.dart';
 import 'package:flutter/material.dart';
+import 'package:flame_fire_atlas/flame_fire_atlas.dart';
 
 class FlameSelectLevelPage extends StatelessWidget {
   const FlameSelectLevelPage({super.key});
@@ -26,6 +28,7 @@ class FlameSelectLevelPage extends StatelessWidget {
 
 class SelectLevelGame extends FlameGame with ScrollDetector {
   static const speed = 2000.0;
+  late FireAtlas _atlas;
 
   @override
   void onScroll(PointerScrollInfo info) {
@@ -39,110 +42,144 @@ class SelectLevelGame extends FlameGame with ScrollDetector {
 
   @override
   FutureOr<void> onLoad() async {
+    // lake , peak , column_1 ,column_2 ,big_vulkano ,chain, wall , floor_1
+    _atlas = await loadFireAtlas('images/vulkanos.fa');
     final bg = SpriteComponent(
       sprite: await loadSprite('stage/01/layers/l2_ground.png'),
       // size: Vector2(1536 , 2048),
     );
     bg.addAll(
       [
-        AlignComponent(
-          alignment: Anchor(0.6, 0.03),
-          child: SelectLevelButton(
-            number: "11",
-            status: LevelStatus.disabled,
-            size: Vector2(100, 100),
+        ...[
+          AlignComponent(
+            alignment: Anchor(0.8, 0.05),
+            child: SpriteComponent(
+                scale: Vector2(0.5, 0.5), sprite: _atlas.getSprite("column_1")
+                // size: Vector2(100, 100),
+                ),
           ),
-        ),
-        AlignComponent(
-          alignment: Anchor(0.56, 0.12),
-          child: SelectLevelButton(
-            number: "10",
-            status: LevelStatus.disabled,
-            size: Vector2(100, 100),
+          AlignComponent(
+            alignment: Anchor(0.6, 0.9),
+            child: SpriteComponent(
+                scale: Vector2(0.6, 0.6), sprite: _atlas.getSprite("column_2")
+                // size: Vector2(100, 100),
+                ),
           ),
-        ),
-        AlignComponent(
-          alignment: Anchor(0.52, 0.21),
-          child: SelectLevelButton(
-            number: "9",
-            status: LevelStatus.disabled,
-            size: Vector2(100, 100),
+          AlignComponent(
+            alignment: Anchor(0.6, 0.9),
+            child: SpriteComponent(
+                scale: Vector2(0.6, 0.6), sprite: _atlas.getSprite("column_2")
+                // size: Vector2(100, 100),
+                ),
           ),
-        ),
-        AlignComponent(
-          alignment: Anchor(0.48, 0.31),
-          child: SelectLevelButton(
-            number: "8",
-            status: LevelStatus.enabled,
-            size: Vector2(100, 100),
+          AlignComponent(
+            alignment: Anchor(0.25, 0.34),
+            child: SpriteComponent(
+                scale: Vector2(0.6, 0.6), sprite: _atlas.getSprite("big_vulkano")
+                // size: Vector2(100, 100),
+                ),
           ),
-        ),
-        AlignComponent(
-          alignment: Anchor(0.59, 0.39),
-          child: SelectLevelButton(
-            number: "7",
-            status: LevelStatus.star2,
-            size: Vector2(100, 100),
+        ],
+        ...[
+          AlignComponent(
+            alignment: Anchor(0.6, 0.03),
+            child: SelectLevelButton(
+              number: "11",
+              status: LevelStatus.disabled,
+              size: Vector2(100, 100),
+            ),
           ),
-        ),
-        AlignComponent(
-          alignment: Anchor(0.6, 0.49),
-          child: SelectLevelButton(
-            number: "6",
-            status: LevelStatus.star1,
-            size: Vector2(100, 100),
+          AlignComponent(
+            alignment: Anchor(0.56, 0.12),
+            child: SelectLevelButton(
+              number: "10",
+              status: LevelStatus.disabled,
+              size: Vector2(100, 100),
+            ),
           ),
-        ),
-        AlignComponent(
-          alignment: Anchor(0.67, 0.58), // 0.5,0.9
-          child: SelectLevelButton(
-            number: "5",
-            size: Vector2(100, 100),
-            status: LevelStatus.star2,
+          AlignComponent(
+            alignment: Anchor(0.52, 0.21),
+            child: SelectLevelButton(
+              number: "9",
+              status: LevelStatus.disabled,
+              size: Vector2(100, 100),
+            ),
           ),
-        ),
-        AlignComponent(
-          alignment: Anchor(0.7, 0.68),
-          child: SelectLevelButton(
-            number: "4",
-            status: LevelStatus.star2,
-            size: Vector2(100, 100),
+          AlignComponent(
+            alignment: Anchor(0.48, 0.31),
+            child: SelectLevelButton(
+              number: "8",
+              status: LevelStatus.enabled,
+              size: Vector2(100, 100),
+            ),
           ),
-        ),
-        AlignComponent(
-          alignment: Anchor(0.53, 0.74),
-          child: SelectLevelButton(
-            number: "3",
-            status: LevelStatus.star3,
-            size: Vector2(100, 100),
+          AlignComponent(
+            alignment: Anchor(0.59, 0.39),
+            child: SelectLevelButton(
+              number: "7",
+              status: LevelStatus.star2,
+              size: Vector2(100, 100),
+            ),
           ),
-        ),
-        AlignComponent(
-          alignment: Anchor(0.46, 0.84),
-          child: SelectLevelButton(
-            number: "2",
-            status: LevelStatus.star3,
-            size: Vector2(100, 100),
+          AlignComponent(
+            alignment: Anchor(0.6, 0.49),
+            child: SelectLevelButton(
+              number: "6",
+              status: LevelStatus.star1,
+              size: Vector2(100, 100),
+            ),
           ),
-        ),
-        AlignComponent(
-          alignment: Anchor(0.44, 0.93), // 0.5,0.9
-          child: SelectLevelButton(
-            number: "1",
-            size: Vector2(100, 100),
-            status: LevelStatus.star3,
+          AlignComponent(
+            alignment: Anchor(0.67, 0.58), // 0.5,0.9
+            child: SelectLevelButton(
+              number: "5",
+              size: Vector2(100, 100),
+              status: LevelStatus.star2,
+            ),
           ),
-          // SpriteComponent(
-          //   sprite: await loadSprite('stage/Parts/buttons/button02.png'),
-          // ),
-        ),
+          AlignComponent(
+            alignment: Anchor(0.7, 0.68),
+            child: SelectLevelButton(
+              number: "4",
+              status: LevelStatus.star2,
+              size: Vector2(100, 100),
+            ),
+          ),
+          AlignComponent(
+            alignment: Anchor(0.53, 0.74),
+            child: SelectLevelButton(
+              number: "3",
+              status: LevelStatus.star3,
+              size: Vector2(100, 100),
+            ),
+          ),
+          AlignComponent(
+            alignment: Anchor(0.46, 0.84),
+            child: SelectLevelButton(
+              number: "2",
+              status: LevelStatus.star3,
+              size: Vector2(100, 100),
+            ),
+          ),
+          AlignComponent(
+            alignment: Anchor(0.44, 0.93), // 0.5,0.9
+            child: SelectLevelButton(
+              number: "1",
+              size: Vector2(100, 100),
+              status: LevelStatus.star3,
+            ),
+            // SpriteComponent(
+            //   sprite: await loadSprite('stage/Parts/buttons/button02.png'),
+            // ),
+          ),
+        ],
       ],
     );
-
+    add(MyParallaxComponent());
     world.add(bg);
     final inset = camera.viewport.virtualSize;
     camera.setBounds(
-      flameExp.Rectangle.fromRect(
+      flameExperimental.Rectangle.fromRect(
         Rect.fromLTWH(
           inset.x / 2,
           inset.y / 2,
@@ -180,15 +217,30 @@ class SelectLevelButton extends AdvancedButtonComponent {
     required this.status,
     super.position,
     super.size,
-  }) : assert(RegExp(r'^[0-9]*$').hasMatch(number),
-            "Oops! Only digits is allowed, your try use $number as String number, try fix it write correct number");
+  })  : assert(RegExp(r'^[0-9]*$').hasMatch(number),
+            "Oops! Only digits is allowed, your try use $number as String number, try fix it write correct number"),
+        super(onPressed: () {});
+
+  @override
+  void onMount() {
+    super.onMount();
+    switch (status) {
+      case LevelStatus.disabled:
+        isDisabled = true;
+      case LevelStatus.enabled:
+      case LevelStatus.star1:
+      case LevelStatus.star2:
+      case LevelStatus.star3:
+        isDisabled = false;
+    }
+  }
 
   @override
   AlignComponent labelAlignContainer =
       AlignComponent(alignment: Anchor(0.41, 0.4));
 
   String getNum(String character) {
-    if (status == LevelStatus.disabled) {
+    if (status == LevelStatus.disabled || status == LevelStatus.enabled) {
       return switch (character) {
         "0" => 'stage/Parts/buttons/num00-gray.png',
         "1" => 'stage/Parts/buttons/num01-gray.png',
@@ -225,12 +277,15 @@ class SelectLevelButton extends AdvancedButtonComponent {
       number.characters.map((i) => Sprite.load(getNum(i))),
     );
     final skinAsset = await Sprite.load(status.asset);
-
+    final labelY =
+        status == LevelStatus.enabled || status == LevelStatus.disabled
+            ? -32.0
+            : 0.0;
     defaultLabel = RectangleComponent(children: [
       ...spriteNum.mapIndexed((index, sprite) => SpriteComponent(
             size: Vector2.all(16),
-            position:
-                Vector2(-number.length * 10, 0) + Vector2(20.0 * index, 0),
+            position: Vector2(-number.length * 10, labelY) +
+                Vector2(20.0 * index, labelY),
             sprite: sprite,
           ))
     ]);
@@ -259,8 +314,21 @@ class SelectLevelButton extends AdvancedButtonComponent {
                 stepTime: 0.016),
           ),
         );
-      downSkin = SpriteComponent(
-          sprite: await Sprite.load('stage/Parts/buttons/button04.png'));
+      downSkin = SpriteComponent(sprite: skinAsset);
     }
+  }
+}
+
+class MyParallaxComponent extends ParallaxComponent<SelectLevelGame> {
+  @override
+  Future<void> onLoad() async {
+    parallax = await game.loadParallax(
+      [
+        ParallaxImageData('stage/01/layers/l1_sky.png'),
+      ],
+      baseVelocity: Vector2(10, 0),
+      velocityMultiplierDelta: Vector2(1.8, 1.0),
+      filterQuality: FilterQuality.none,
+    );
   }
 }
